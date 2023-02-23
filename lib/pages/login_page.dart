54eee10819d0854279f07e9c412a5750e8676999
Arnav Shah <arnavkshah2003@gmail.com>
2023-02-24 02:51:53 +0530

    renamed:    assets/login.png -> assets/images/login.png 	modified:   lib/main.dart     modified:   lib/pages/login_page.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,13 +6,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-          textScaleFactor: 3.0,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset("assets/images/login.png"),
+            Text("Welcome",
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold))
+          ],
+        ));
   }
 }
